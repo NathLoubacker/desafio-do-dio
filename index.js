62c30ff7519bef21926fbca3b8b357c5dc65f902
NathLoubacker <nath.loubacker@gmail.com>
2025-03-22 22:20:24 -0300

    Primeiro projeto

---
+++ index.js
@@ -0,0 +1,37 @@
+let nome = "heroi"
+let experiencia = 3500
+let nível = ""
+if (experiencia < 1000) {
+    nivel = "Ferro";
+} else if (experiencia >= 1001 && experiencia <= 2000) {
+    nivel = "Bronze";
+} else if (experiencia >= 2001 && experiencia <= 5000) {
+    nivel = "Prata";
+} else if (experiencia >= 5001 && experiencia <= 7000) {
+    nivel = "Ouro";
+} else if (experiencia >= 7001 && experiencia <= 8000) {
+    nivel = "Platina";
+} else if (experiencia >= 8001 && experiencia <= 9000) {
+    nivel = "Ascendente";
+} else if (experiencia >= 9001 && experiencia <= 10000) {
+    nivel = "Imortal";
+} else if (experiencia >= 10001) {
+    nivel = "Radiante"
+}
+
+console.log("O Herói de nome goku " + "está no nível de " + experiencia );
+
+
+let continuar = true;
+
+while (continuar) {
+    // Solicitar o nome e a XP do herói
+    let nome = "goku"
+    let experiencia = 3500
+
+    console.log("O Herói de nome goku " + "está no nível de " + experiencia );
+continuar = false;
+}
+
+
+
